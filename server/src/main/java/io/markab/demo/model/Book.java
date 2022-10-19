b1d8e6c1e4e4f5b54744bfe6bf0a5d19f210e0ec
itechdom <oalghnmi@gmail.com>
2022-10-19 22:08:27 +0300

    collection operations

--- server/src/main/java/io/markab/demo/model/Book.java
+++ server/src/main/java/io/markab/demo/model/Book.java
@@ -3,6 +3,7 @@ package io.markab.demo.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Book implements Serializable {
@@ -19,14 +20,18 @@ public class Book implements Serializable {
         this.authorId = authorId;
     }
 
-    //array.asList isn't thread safe
-    //returns a fixed size list
+    // array.asList isn't thread safe
+    // returns a fixed size list
     public static List<Book> books = Arrays.asList(
             new Book("book-1", "Harry Potter and the Philosopher's Stone", 223, "author-1"),
             new Book("book-2", "Moby Dick", 635, "author-2"),
             new Book("book-3", "Interview with the vampire", 371, "author-3"));
 
     public static ArrayList<Book> booksChangeable = new ArrayList<Book>();
+
+    public static ArrayList<Author> authorChangeable = new ArrayList<Author>();
+
+    public static HashMap<String, Author> authorLookup = new HashMap<String, Author>();
 
     public static Book getById(String id) {
         return books.stream().filter(book -> book.getId().equals(id)).findFirst().orElse(null);
--- server/src/main/java/io/markab/demo/service/SenderService.java
+++ server/src/main/java/io/markab/demo/service/SenderService.java
@@ -1,12 +1,18 @@
 package io.markab.demo.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.markab.demo.model.Author;
 import io.markab.demo.model.Book;
 
 @Service
@@ -29,13 +35,17 @@ public class SenderService implements Runnable {
     @Override
     public void run() {
         Book.booksChangeable.add(new Book("hello", "BOOKStat", 0, "10"));
-        System.out.println(Thread.currentThread().getName());
-        System.out.println(Book.booksChangeable.toString() + "----" + Thread.currentThread().getName());
+        Book.authorChangeable.add(new Author("author", "author-a", "author-a"));
+        List<Book> mappedBooks = Book.booksChangeable.stream().map((book) -> book)
+                .collect(Collectors.toList());
         System.out.println("Sending message...");
         try {
             Thread.sleep(5000);
-            Book.booksChangeable.remove(0);
-            Book.booksChangeable.add(0, new Book("added", "added", 100, "added"));
+            // Book.booksChangeable.remove(0);
+            // Book.booksChangeable.add(0, new Book("added", "added", 100, "added"));
+            System.out.println("----------------------");
+            mappedBooks.forEach((author) -> System.out.println(author));
+            System.out.println("----------------------");
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
