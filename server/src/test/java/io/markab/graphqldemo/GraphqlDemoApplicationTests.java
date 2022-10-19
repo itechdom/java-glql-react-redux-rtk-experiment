3466f47622f5e5ea14a5ac4d3345b5785ef9ea35
itechdom <oalghnmi@gmail.com>
2022-10-19 09:57:13 +0300

    client and server

---
+++ server/src/test/java/io/markab/graphqldemo/GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.markab.graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
