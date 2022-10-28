08a4b027431336151f6cfd4f2b44c541ecba364c
itechdom <oalghnmi@gmail.com>
2022-10-28 10:35:28 +0300

    Node list

--- server/src/main/java/io/markab/demo/GlqlApplication.java
+++ server/src/main/java/io/markab/demo/GlqlApplication.java
@@ -24,6 +24,23 @@ import io.markab.demo.service.SenderService;
 @RestController
 public class GlqlApplication {
 
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
 	@Autowired
 	SenderService senderService;
 
@@ -85,8 +102,48 @@ public class GlqlApplication {
 		}
 	}
 
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		int carry = 0;
+		ListNode resultNode = new ListNode();
+		// check l1.next, l2.next whichever contains next move to them
+		// if last index, then add a new node
+		// else add carry to current value
+		while (l1 != null && l2 != null) {
+			int val1 = l1.val;
+			int val2 = l2.val;
+			int sum = val1 + val2 + carry;
+			carry = sum % 9;
+			if (carry != 0) {
+				resultNode.val = sum - 10;
+			} else {
+				resultNode.val = sum;
+			}
+			l1 = l1.next;
+			l2 = l2.next;
+			resultNode.next = new ListNode();
+		}
+		return resultNode;
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(GlqlApplication.class, args);
+		ListNode l3 = new ListNode(5);
+		ListNode l2 = new ListNode(4, l3);
+		ListNode l1 = new ListNode(3, l2);
+
+		ListNode l6 = new ListNode(7);
+		ListNode l5 = new ListNode(8, l6);
+		ListNode l4 = new ListNode(9, l5);
+
+		ListNode result = addTwoNumbers(l1, l4);
+
+		while (result != null) {
+			System.out.println(result.val);
+			result = result.next;
+		}
+
+		// System.out.println(result.toString());
+
+		// SpringApplication.run(GlqlApplication.class, args);
 	}
 
 }
